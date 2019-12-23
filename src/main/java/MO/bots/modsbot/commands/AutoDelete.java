765e3133d07e34f3202418eb25c338325b1674d0
IcosahedralDice <etomnz@gmail.com>
2019-12-23 17:01:20 +1300

    autodelete logging

--- src/main/java/MO/bots/modsbot/commands/AutoDelete.java
+++ src/main/java/MO/bots/modsbot/commands/AutoDelete.java
@@ -1,8 +1,15 @@
 package MO.bots.modsbot.commands;
 
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.time.Instant;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
 
+import MO.bots.shared.SheetsIntegration;
+import net.dv8tion.jda.core.entities.Guild;
 import net.dv8tion.jda.core.entities.Message;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
@@ -16,19 +23,34 @@ public class AutoDelete extends ListenerAdapter {
 	}
 	
 	public void onMessageReceived (MessageReceivedEvent event) {
-		TimerTask t = new DeleteMessageTask(event.getMessage());
+		TimerTask t = new DeleteMessageTask(event.getMessage(), event.getGuild());
 		mainTimer.schedule(t, delay);
 	}
 }
 
 class DeleteMessageTask extends TimerTask {
-	Message messageToDelete;
+	private Guild messageGuild;
+	private Message messageToDelete;
+	static final long LOG_CHANNEL_ID = 559964001724006400L;
+	static final String LOG_SHEET_ID = "12YgBc7wd_N4x-FKW0qmbJKcXl2XshBcbWdsF1ZMSB2E";
 	
-	public DeleteMessageTask(Message m) {
+	public DeleteMessageTask(Message m, Guild g) {
 		this.messageToDelete = m;
+		this.messageGuild = g;
 	}
 	
 	public void run() {
 		this.messageToDelete.delete().queue();
+		List<Object> row = Arrays.asList((Object) Instant.now().toString(), 
+				(Object) this.messageToDelete.getAuthor().toString(), 
+				this.messageToDelete.getAuthor().getId(), 
+				this.messageToDelete.getContentRaw());
+		try {
+			SheetsIntegration.appendRow(LOG_SHEET_ID, row, "autodelete log");
+		} catch (GeneralSecurityException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
