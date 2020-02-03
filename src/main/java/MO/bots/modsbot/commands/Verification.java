40a20c130e0141e403dfe667f464a2f41da0ca83
IcosahedralDice <etomnz@gmail.com>
2020-02-03 13:36:31 +1300

    bug fixes 2

--- src/main/java/MO/bots/modsbot/commands/Verification.java
+++ src/main/java/MO/bots/modsbot/commands/Verification.java
@@ -3,6 +3,7 @@ package MO.bots.modsbot.commands;
 import java.time.Instant;
 
 import net.dv8tion.jda.core.entities.Message;
+import net.dv8tion.jda.core.entities.Role;
 import net.dv8tion.jda.core.events.message.react.MessageReactionAddEvent;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
 
@@ -15,21 +16,24 @@ public class Verification extends ListenerAdapter {
 	@Override
     public void onMessageReactionAdd(MessageReactionAddEvent event) {
 		if (event.getMessageIdLong() == MESSAGE_ID) {
-			event.getGuild().getController().removeRolesFromMember(
-					event.getMember(), event.getGuild().getRolesByName("unverified", true)).complete();
-			
 			Message m = event.getTextChannel().getMessageById(event.getMessageId()).complete();
 			m.getReactions().get(0).removeReaction(event.getUser()).complete();
 			
-			event.getGuild().getTextChannelById(WELCOME_CHANNEL_ID).sendMessage(
-					"Welcome to the Mathematical Olympiads Discord server "
-					+ event.getUser().getAsMention() 
-					+ "! Check out the self-assignable roles in " 
-					+ event.getGuild().getTextChannelById(ROLES_ID).getAsMention()
-					+ "and enjoy your time here. :smile:").complete();
+			Role unverified = event.getGuild().getRolesByName("unverified", true).get(0);
 			
-			System.out.println("Verified " + event.getUser().toString() + " at " + Instant.now().toString());
-			
+			if (event.getMember().getRoles().contains(unverified)) {
+				event.getGuild().getController().removeRolesFromMember(
+						event.getMember(), event.getGuild().getRolesByName("unverified", true)).complete();
+				
+				event.getGuild().getTextChannelById(WELCOME_CHANNEL_ID).sendMessage(
+						"Welcome to the Mathematical Olympiads Discord server "
+						+ event.getUser().getAsMention() 
+						+ "! Check out the self-assignable roles in " 
+						+ event.getGuild().getTextChannelById(ROLES_ID).getAsMention()
+						+ " and enjoy your time here. :smile:").complete();
+				
+				System.out.println("Verified " + event.getUser().toString() + " at " + Instant.now().toString());
+			}
 		}
 		
 	}
